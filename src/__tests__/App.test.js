cd35902397be2598e89d3cd1bd6fedba0012a5c5
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-16 22:08:36 +0100

    added failing test for rendering logo component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -9,6 +9,10 @@ describe('App', () => {
     expect(app).toMatchSnapshot();
   });
 
+  it('renders a Logo component', () => {
+    expect(app.find('Logo').exists()).toBe(true);
+  });
+
   describe('Initializes state successfully', () =>{
     it('initializes results as an array of the event.json', () => {
       expect(app.state('results')).toHaveProperty("event.areas-covered")
