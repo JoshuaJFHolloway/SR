d362642ae77ace53ed4f00495ffce579258c7170
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-17 12:02:46 +0100

    created failing tests for new toggle handler

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -3,30 +3,99 @@ import { shallow } from 'enzyme';
 import App from '../components/App';
 
 describe('App', () => {
-  const app = shallow(<App/>);
 
-  it('renders correctly', () => {
-    expect(app).toMatchSnapshot();
+  let app = shallow(<App/>);
+
+  describe('Rendering components', () => {
+    it('renders correctly', () => {
+      expect(app).toMatchSnapshot();
+    });
+
+    it('renders a Logo component', () => {
+      expect(app.find('Logo').exists()).toBe(true);
+    });
+
+    it('renders a Toolbar component', () => {
+      expect(app.find('Toolbar').exists()).toBe(true);
+    });
+
+    // describe('Events not clicked', () => {
+    //   beforeEach(() => {
+    //     app = shallow(<App />);
+    //   });
+    //   it('does not render another Toolbar component', () => {
+    //     expect(app.find('Toolbar').exists()).toBe(true);
+    //   });
+    // });
+
+    // describe('Events is clicked', () => {
+    //   beforeEach(() => {
+    //     app.setState({eventsToolbarClicked: true})
+    //   });
+    //
+    //   it('renders renders another Toolbar component', () => {
+    //     expect(app.find('Toolbar').exists()).toBe(true);
+    //   });
+    // });
+
   });
 
-  it('renders a Logo component', () => {
-    expect(app.find('Logo').exists()).toBe(true);
-  });
-
-  it('renders a toolbar component', () => {
-    expect(app.find('Toolbar').exists()).toBe(true);
-  });
-
-  describe('Initializes state successfully', () =>{
+  describe('initializes state successfully', () =>{
     it('initializes results that include the event.json', () => {
       expect(app.state('results')).toHaveProperty("event.areas-covered");
     });
   });
 
-  describe('Props successfully passing down to the Logo component', () =>{
+  describe('props successfully passing down to the Logo component', () =>{
     it('passes this.state.images.logo', () => {
       expect(app.find('Logo').prop('logo'))
     });
   });
+
+  describe('updating the state', () => {
+    beforeEach(() => {
+      app = shallow(<App />);
+    });
+
+    it('should update the state when events is clicked', () => {
+      const event = { target: { className: 'eventsToolbarClicked', value: true } };
+      app.instance().handleClickToggle(event);
+      expect(app.state('params')).toEqual({"eventsNestedBarClicked": false, "eventsToolbarClicked": true});
+    });
+
+    it("should update the state when the first 'events' on the nested toolbar is clicked", () => {
+      const event2 = { target: { className: 'eventsToolbarClicked', value: true } };
+      app.instance().handleClickToggle(event2);
+      const event = { target: { className: 'eventsNestedBarClicked', value: true } };
+      app.instance().handleClickToggle(event);
+      expect(app.state('params')).toEqual({"eventsNestedBarClicked": true, "eventsToolbarClicked": true});
+    });
+
+    it('should change both states to false if eventsToolbar is false', () => {
+      const event1 = { target: { className: 'eventsToolbarClicked', value: true } };
+      app.instance().handleClickToggle(event1);
+      const event2 = { target: { className: 'eventsNestedBarClicked', value: true } };
+      app.instance().handleClickToggle(event2);
+      const event3 = { target: { className: 'eventsToolbarClicked', value: false } };
+      app.instance().handleClickToggle(event3);
+      expect(app.state('params')).toEqual({"eventsNestedBarClicked": false, "eventsToolbarClicked": false});
+    })
+
+
+
+    //
+    // it('saves output state when button is clicked', () => {
+    //   form.instance().handleDisplayClick();
+    //   expect(form.state('savedFirstScaleScore')).toEqual(99);
+    // });
+    //
+    // it('should change the buttonClicked state to true', () => {
+    //   form = shallow(<Form />);
+    //   form.instance().handleDisplayClick();
+    //   expect(form.state('buttonClicked')).toEqual(true);
+    // });
+  });
+
+
 
 });
