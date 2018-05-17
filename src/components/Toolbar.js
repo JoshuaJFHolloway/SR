d667678c6bafdebaa4b4aed342971bae45a1e805
Joshua Holloway <josh.holloway@live.co.uk>
2018-05-17 10:06:03 +0100

    :art: css on toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
-const Toolbar = () => {
-  return(
-    <div>
+const toolbarStyle = { display: 'inline-block', verticalAlign: 'top', width: '19%' };
 
+const Toolbar = (props) => {
+  return (
+    <div style={toolbarStyle} >
+      <div style={toolbarStyle} >
+        {props.tab1}
+      </div>
+      <div onClick={props.eventHandler} className={props.tab2} style={toolbarStyle}>
+        {props.tab2}
+      </div>
     </div>
   )
 };
